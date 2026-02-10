3ac92a142910e8353b229ba9171e8976c5082083
Oscar Riojas <oscarriojas@gmail.com>
2026-02-10 13:56:23 -0500

    feat/ create rust initial code

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+use actix_cors::Cors;
+use log::info;
+
+use rust_backend::handlers::{analysis_handler, decode_handler};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenvy::dotenv().ok();
+    env_logger::init();
+
+    let server_address = "127.0.0.1:8080";
+
+    info!("🚀 Servidor web iniciando en http://{}", server_address);
+
+    HttpServer::new(|| {
+        let cors = Cors::permissive();
+
+        App::new()
+            .wrap(cors)
+            .route("/decode", web::post().to(decode_handler))
+            .route("/analysis", web::post().to(analysis_handler))
+    })
+    .bind(server_address)?
+    .run()
+    .await
 }
